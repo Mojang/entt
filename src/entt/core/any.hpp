6c08ff6801120327270d22f7f2cd9ae9b126807a
Eric Jia <v-ericjia@microsoft.com>
2021-11-10 14:25:57 +0800

    m

--- src/entt/core/any.hpp
+++ src/entt/core/any.hpp
@@ -44,9 +44,6 @@ class basic_any {
 
     template<typename Type>
     static const void *basic_vtable([[maybe_unused]] const operation op, [[maybe_unused]] const basic_any &from, [[maybe_unused]] const void *to) {
-        if (to) {
-            return nullptr;
-        }
         static_assert(!std::is_same_v<Type, void> && std::is_same_v<std::remove_reference_t<std::remove_const_t<Type>>, Type>, "Invalid type");
         const Type *instance = nullptr;
 
@@ -57,49 +54,54 @@ class basic_any {
         }
 
         switch(op) {
+        case operation::destroy:
+            if constexpr (in_situ<Type>) {
+                instance->~Type();
+            }
+            else if constexpr (std::is_array_v<Type>) {
+                delete[] instance;
+            }
+            else {
+                delete instance;
+            }
+            break;
+        case operation::get:
+            return instance;
+        }
+        if (to) {
         case operation::copy:
-            if constexpr(std::is_copy_constructible_v<Type>) {
+            if constexpr (std::is_copy_constructible_v<Type>) {
                 ENTT_ASSERT(instance, "Unexpected nullptr");
-                static_cast<basic_any *>(const_cast<void *>(to))->initialize<Type>(*instance);
+                static_cast<basic_any*>(const_cast<void*>(to))->initialize<Type>(*instance);
             }
             break;
         case operation::move:
-            if constexpr(in_situ<Type>) {
-                if(from.mode == policy::owner) {
-                    return new(&static_cast<basic_any *>(const_cast<void *>(to))->storage) Type{std::move(*const_cast<Type *>(instance))};
+            if constexpr (in_situ<Type>) {
+                if (from.mode == policy::owner) {
+                    return new(&static_cast<basic_any*>(const_cast<void*>(to))->storage) Type{ std::move(*const_cast<Type*>(instance)) };
                 }
             }
 
-            return (static_cast<basic_any *>(const_cast<void *>(to))->instance = std::exchange(const_cast<basic_any &>(from).instance, nullptr));
+            return (static_cast<basic_any*>(const_cast<void*>(to))->instance = std::exchange(const_cast<basic_any&>(from).instance, nullptr));
         case operation::transfer:
-            if constexpr(std::is_move_assignable_v<Type>) {
-                *const_cast<Type *>(instance) = std::move(*static_cast<Type *>(const_cast<void *>(to)));
+            if constexpr (std::is_move_assignable_v<Type>) {
+                *const_cast<Type*>(instance) = std::move(*static_cast<Type*>(const_cast<void*>(to)));
                 return to;
             }
             [[fallthrough]];
         case operation::assign:
-            if constexpr(std::is_copy_assignable_v<Type>) {
-                *const_cast<Type *>(instance) = *static_cast<const Type *>(to);
+            if constexpr (std::is_copy_assignable_v<Type>) {
+                *const_cast<Type*>(instance) = *static_cast<const Type*>(to);
                 return to;
             }
             break;
-        case operation::destroy:
-            if constexpr(in_situ<Type>) {
-                instance->~Type();
-            } else if constexpr(std::is_array_v<Type>) {
-                delete[] instance;
-            } else {
-                delete instance;
-            }
-            break;
         case operation::compare:
-            if constexpr(!std::is_function_v<Type> && !std::is_array_v<Type> && is_equality_comparable_v<Type>) {
-                return *static_cast<const Type *>(instance) == *static_cast<const Type *>(to) ? to : nullptr;
-            } else {
+            if constexpr (!std::is_function_v<Type> && !std::is_array_v<Type> && is_equality_comparable_v<Type>) {
+                return *static_cast<const Type*>(instance) == *static_cast<const Type*>(to) ? to : nullptr;
+            }
+            else {
                 return (instance == to) ? to : nullptr;
             }
-        case operation::get:
-            return instance;
         }
 
         return nullptr;
